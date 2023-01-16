10732636e4b63a134704ef42307411d5391c84f9
binarygit <inscription4@outlook.com>
2023-01-16 16:11:46 +0545

    Create the GoRailsScraper class to scrape jobs from gorails and create a PageGenerator class to generate the HTML

--- main.rb
+++ main.rb
@@ -1,19 +1,47 @@
 require 'nokogiri'
 require 'open-uri'
 require 'debug'
+require 'erb'
 
-base_url = 'https://jobs.gorails.com'
-doc = Nokogiri::HTML( URI.open(base_url) ) 
-job_postings = doc.css('body > div li')
-job_urls = []
-titles = []
-job_postings.each do |job|
-  anchor_tag = job.at_css('a')
-  job_urls << base_url + anchor_tag['href']
+class GoRailsScraper
+  attr_reader :base_url
 
-  title_element = job.at_css('h3')
-  titles << title_element.text
+  def initialize
+    @base_url = 'https://jobs.gorails.com'
+  end
+
+  def scrape
+    doc = Nokogiri::HTML( URI.open(base_url) )
+    job_postings = doc.css('body > div li')
+    jobs = []
+
+    job_postings.each do |job|
+      anchor_tag = job.at_css('a')
+      hash = {}
+      hash[:href] = base_url + anchor_tag['href']
+
+      title_element = job.at_css('h3')
+      hash[:title] = title_element.text
+      jobs << hash
+
+      misc = job.at_css('div.w-full.truncate').css('div')[3]
+      hash[:misc] = misc.text.strip.gsub("\n", " ")
+    end
+    jobs
+  end
 end
 
-p job_urls
-p titles
+class PageGenerator
+  def initialize
+    @go_rails_jobs = GoRailsScraper.new.scrape
+  end
+
+  def generate
+    template = ERB.new File.read('index.html.erb')
+    html = template.result(binding)
+    File.write('index.html', html)
+  end
+end
+
+page_generator = PageGenerator.new
+page_generator.generate
