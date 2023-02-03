862f36594443a1097f31f019e38520c021985d0b
binarygit <inscription4@outlook.com>
2023-02-03 23:19:18 +0545

    Delete previous implementation of main.rb

--- main.rb
+++ main.rb
@@ -3,27 +3,3 @@ require 'erb'
 Dir.new('./scrapers').children.each do |file|
   require_relative "./scrapers/#{file}"
 end
-
-class PageGenerator
-  def generate(page)
-    jobs = page[:jobs]
-    template = ERB.new File.read('templates/index.html.erb')
-    html = template.result(binding)
-    File.write("templates/#{page[:file_name]}", html)
-  end
-end
-
-gorails_jobs = GoRailsScraper.new.scrape
-wwr_jobs = WeWorkRemotelyScraper.new.scrape
-ror_jobs = RubyOnRailsJobsScraper.new.scrape
-ruby_on_remote_jobs = RubyOnRemoteScraper.new.scrape
-pages = [{ jobs: gorails_jobs, file_name: 'gorails.html' },
-         { jobs: wwr_jobs, file_name: 'weworkremotely.html' },
-         { jobs: ror_jobs, file_name: 'rorjobs.html' },
-         { jobs: ruby_on_remote_jobs, file_name: 'rubyonremote.html' },
-]
-
-page_generator = PageGenerator.new
-pages.each do |page|
-  page_generator.generate(page)
-end
